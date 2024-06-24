eded8d5332d94c29719dad118435e400c7d57122
KrtiinSharma <krtiinsharma2415@gmail.com>
2024-06-24 11:47:44 +0530

    configuration for the deployment

---
+++ backend/dist/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import dotenv from "dotenv";
+import { app, server } from "./socket/socket.js";
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+app;
+app.use(cookieParser()); //for parsing cookies
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+server.listen(PORT, () => {
+    console.log(`server is running on port ${PORT} `);
+});
+//TODO Add socket.io to the server
+//TODO configure this server for the deployment
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,18 +10,21 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-
-app
-app.use(cookieParser());//for parsing cookies
-app.use(express.json());
+app.use(cookieParser()); // for parsing cookies
+app.use(express.json()); // for parsing application/json
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, () => {
-  console.log(`server is running on port ${PORT} `);
-});
+if (process.env.NODE_ENV !== "development") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
-//TODO Add socket.io to the server
-//TODO configure this server for the deployment
+server.listen(PORT, () => {
+	console.log("Server is running on port " + PORT);
+});
