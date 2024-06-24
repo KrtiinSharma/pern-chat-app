688daa0cfee41fd3f784c9ea56177ea0b96105c0
KrtiinSharma <krtiinsharma2415@gmail.com>
2024-06-24 11:35:56 +0530

    socket.io implemented

--- backend/src/controllers/message.controller.ts
+++ backend/src/controllers/message.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../db/prisma.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req: Request, res: Response) => {
 	try {
@@ -50,11 +51,11 @@ export const sendMessage = async (req: Request, res: Response) => {
 		}
 
 		// Socket io will go here
-		//const receiverSocketId = getReceiverSocketId(receiverId);
+		const receiverSocketId = getReceiverSocketId(receiverId);
 
-		//if (receiverSocketId) {
-		//	io.to(receiverSocketId).emit("newMessage", newMessage);
-		//}
+		if (receiverSocketId) {
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
 
 		res.status(201).json(newMessage);
 	} catch (error: any) {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,19 +5,20 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 import dotenv from "dotenv";
+import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
-const app = express();
 
+app
 app.use(cookieParser());//for parsing cookies
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server is running on port ${PORT} `);
 });
 
